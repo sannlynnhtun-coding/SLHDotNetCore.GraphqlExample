11ddab36d5151629b4a67e56794daaaaa75fcae6
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-08 15:58:42 +0630

    [Complete] Database

---
+++ SLHDotNetCore.GraphqlExample/Database/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Configuration;
+using SLHDotNetCore.GraphqlExample.Features.Blog;
+using SLHDotNetCore.GraphqlExample.Features.Fruit;
+using System.IO;
+
+namespace SLHDotNetCore.GraphqlExample.Database;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+    public DbSet<FruitModel> Fruits { get; set; }
+}
