735800eb3c5899648c15c140e9fa519f65a81356
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-08 15:59:09 +0630

    [Complete] Fruit CRUD

---
+++ SLHDotNetCore.GraphqlExample/Features/Fruit/FruitModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SLHDotNetCore.GraphqlExample.Features.Fruit;
+
+[Table("Tbl_Fruit")]
+public class FruitModel
+{
+    [Key]
+    public int Fruit_Id { get; set; }
+    public string Fruit_Name { get; set; }
+}
